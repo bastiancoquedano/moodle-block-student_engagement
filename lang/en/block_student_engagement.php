8637831a7c4231f2a65b915c6565c2b89d8bdc7f
Bastian Coquedano <bastiancoquedano@MacBook-Air-de-Bastian.local>
2026-03-09 01:46:51 -0300

    [FEAT] Se agregan archivos de idioma en y es

---
+++ lang/en/block_student_engagement.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_student_engagement', language 'en'.
+ *
+ * @package    block_student_engagement
+ * @copyright  2026 Bastian Coquedano
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Student Engagement';
+$string['student_engagement:addinstance'] = 'Add a new Student Engagement block';
+$string['student_engagement:myaddinstance'] = 'Add a new Student Engagement block to Dashboard';
+$string['student_engagement:view'] = 'View Student Engagement block';
+$string['contentnotready'] = 'Engagement metrics are not available yet.';
+$string['nopermissions'] = 'You do not have permission to view this block.';
